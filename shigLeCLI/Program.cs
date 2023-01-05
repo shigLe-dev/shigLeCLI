77e165f9079b35cceff56b34b7104c0c49c0203f
KYapp <kukiyomenaisapp@gmail.com>
2023-01-05 20:16:47 +0900

    Spectreテスト

--- shigLeCLI/Program.cs
+++ shigLeCLI/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace shigLeCLI;
+﻿using Spectre.Console;
+
+namespace shigLeCLI;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Table table = new Table();
+
+        table.AddColumn("name");
+        table.AddColumn("hoge2");
+        table.AddRow("foo", "bar");
+        table.AddRow("foo2", "bar2");
+
+        AnsiConsole.Write(table);
     }
 }
